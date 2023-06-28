4ca01da1f792b5b81c717d94cdd89fa04e0b7d1a
Tobias Zwick <newton@westnordost.de>
2023-06-28 13:32:55 +0200

    update kotlin gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.4.2")
-        classpath(kotlin("gradle-plugin", version = "1.8.0"))
+        classpath(kotlin("gradle-plugin", version = "1.8.22"))
     }
 }
 
