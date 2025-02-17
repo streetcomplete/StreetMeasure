1b8a0e38d66f83cf5bb7f4366490d7769ee7080c
Tobias Zwick <newton@westnordost.de>
2025-02-17 13:39:15 +0100

    update agp, dependencies, compileSdk, targetSdk, target/source compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "de.westnordost.streetmeasure"
-    compileSdk = 34
+    compileSdk = 35
 
     signingConfigs {
         create("release") {
@@ -19,7 +19,7 @@ android {
     defaultConfig {
         applicationId = "de.westnordost.streetmeasure"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 5
         versionName = "1.4"
     }
@@ -39,31 +39,31 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "21"
     }
 }
 
 dependencies {
-    val kotlinxCoroutinesVersion = "1.8.1"
+    val kotlinxCoroutinesVersion = "1.10.1"
 
     // core android stuff
-    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     // coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinxCoroutinesVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$kotlinxCoroutinesVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:$kotlinxCoroutinesVersion")
 
     // measuring distance with AR
-    implementation("com.google.ar:core:1.45.0")
+    implementation("com.google.ar:core:1.47.0")
     implementation("com.google.ar.sceneform:core:1.17.1")
 }
 
