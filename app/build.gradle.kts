6dc9a79a701571bc1025ca1f7e9b86e884ead90e
Tobias Zwick <newton@westnordost.de>
2025-02-25 14:11:55 +0100

    version up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "de.westnordost.streetmeasure"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5"
     }
 
     buildTypes {
